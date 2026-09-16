1b9b345dede5b20c46ef7176f3cade9ddce5f3a1
Aishik-2005 <banerjeeaishik778@gmail.com>
2026-09-16 06:40:17 +0530

    Time: 387 ms (25.72%), Space: 42.3 MB (98.42%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,65 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                // Find empty cell
+                if (board[row][col] == '.') {
+
+                    // Try numbers 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            board[row][col] = num;
+
+                            // Recursively solve
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // If wrong, undo
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char num) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Check row
+            if (board[row][i] == num) {
+                return false;
+            }
+
+            // Check column
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check 3x3 box
+            int r = 3 * (row / 3) + i / 3;
+            int c = 3 * (col / 3) + i % 3;
+
+            if (board[r][c] == num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
